ccda26c87308de84dbef91abc66dedbee88d49a0
IMMORTAL10github <146534604+IMMORTAL10github@users.noreply.github.com>
2024-08-21 21:56:10 +0530

    Delete cognizant npn stage2 Excercise/Week4_and_5 directory

--- cognizant npn stage2 Excercise/Week4_and_5/Exercise 8/BookstoreAPI/src/main/java/com/bookstore/dto/CustomerDTO.java
+++
@@ -1,7 +0,0 @@
-package bookstore.bookstoreapi_7.dto;
-
-public class CustomerDTO {
-    private Long id;
-    private String name;
-    private String email;
-}
--- cognizant npn stage2 Excercise/Week4_and_5/Exercise 8/BookstoreAPI/src/main/java/com/bookstore/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package bookstore.bookstoreapi_6.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
