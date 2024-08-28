a10e6bbc2e60e24f3f24f70ab6c9513139d6d9b8
IMMORTAL10github <146534604+IMMORTAL10github@users.noreply.github.com>
2024-08-28 09:17:27 +0530

    Add files via upload

---
+++ cognizant npn stage2 Excercise/Week4_and_5/Exercise 12/BookstoreAPI/src/main/java/com/bookstoreapi/dto/BookDTO.java
@@ -0,0 +1,12 @@
+package com.bookstoreapi.dto;
+
+import lombok.Data;
+
+@Data
+public class BookDTO
+{
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+}
---
+++ cognizant npn stage2 Excercise/Week4_and_5/Exercise 9/BookstoreAPI/src/main/java/com/bookstore/dto/CustomerDTO.java
@@ -0,0 +1,7 @@
+package com.bookstore.dto;
+
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
